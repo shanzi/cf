23a48212c94f28c27eb57dc27080a80f8e7b50a7
chase <yun.er.run@gmail.com>
2021-03-01 23:35:55 +0900

    Solve 1492B

--- templates/main.cpp
+++ templates/main.cpp
@@ -6,9 +6,19 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
+
+void solve() {
+    
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin>>tc;
+    for (int i = 0; i < tc; ++i) {
+        solve();
+    }
     return 0;
 }
